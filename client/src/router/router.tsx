372fe5cfb1181c3ca5b8294bfae13c4b26548ed5
Vasyl Pryimak <vasyl.pryimak.dev@gmail.com>
2026-06-29 19:54:47 +0300

    feat(auth): add sign-in page with email and Google authentication

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignUpPage from "../pages/SignUpPage";
 import OnboardingPage from "../pages/OnboardingPage";
 import AuthCallbackPage from "../pages/AuthCallbackPage";
+import SignInPage from "../pages/SignInPage";
 
 export function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/auth/v1/callback" element={<AuthCallbackPage />} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
         <Route path="*" element={<Navigate to="/signup" replace />} />
